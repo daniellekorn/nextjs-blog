317e48800f6835171c0c67ead4e25f73d30c7a6c
Danielle Korn <daniella.korn@gmail.com>
2023-05-23 16:06:25 +0300

    start to style

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,57 +1,57 @@
-import getFormattedDate from "@/lib/getFormattedDate"
-import { getSortedPostsData, getPostData } from "@/lib/posts"
-import { notFound } from "next/navigation"
-import Link from "next/link"
+import getFormattedDate from "@/lib/getFormattedDate";
+import { getSortedPostsData, getPostData } from "@/lib/posts";
+import { notFound } from "next/navigation";
+import Link from "next/link";
 
 export function generateStaticParams() {
-    const posts = getSortedPostsData()
+  const posts = getSortedPostsData();
 
-    return posts.map((post) => ({
-        postId: post.id
-    }))
+  return posts.map((post) => ({
+    postId: post.id,
+  }));
 }
 
 export function generateMetadata({ params }: { params: { postId: string } }) {
+  const posts = getSortedPostsData();
+  const { postId } = params;
 
-    const posts = getSortedPostsData()
-    const { postId } = params
+  const post = posts.find((post) => post.id === postId);
 
-    const post = posts.find(post => post.id === postId)
-
-    if (!post) {
-        return {
-            title: 'Post Not Found'
-        }
-    }
-
+  if (!post) {
     return {
-        title: post.title,
-    }
+      title: "Post Not Found",
+    };
+  }
+
+  return {
+    title: post.title,
+  };
 }
 
 export default async function Post({ params }: { params: { postId: string } }) {
+  const posts = getSortedPostsData();
+  const { postId } = params;
 
-    const posts = getSortedPostsData()
-    const { postId } = params
+  if (!posts.find((post) => post.id === postId)) notFound();
 
-    if (!posts.find(post => post.id === postId)) notFound()
+  const { title, date, contentHtml } = await getPostData(postId);
 
-    const { title, date, contentHtml } = await getPostData(postId)
+  const pubDate = getFormattedDate(date);
 
-    const pubDate = getFormattedDate(date)
-
-    return (
-        <main>
-            <h1>{title}</h1>
-            <p>
-                {pubDate}
-            </p>
-            <article>
-                <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-                <p>
-                    <Link href="/">← Back to home</Link>
-                </p>
-            </article>
-        </main>
-    )
+  return (
+    <main className="flex container mx-auto">
+      <div className="flex-col container mx-auto p-10">
+        <h1 className="prose md:prose-lg lg:prose-xl mx-auto">{title}</h1>
+        <p className="prose md:prose-sm lg:prose-md mx-auto">{pubDate}</p>
+        <article className="prose md:prose-md lg:prose-lg mt-8 mx-auto">
+          <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+          <p>
+            <Link href="/" className="prose prose-sm">
+              ← Back to home
+            </Link>
+          </p>
+        </article>
+      </div>
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    'accentColor': '#818cf8', // Indigo 400
   },
   plugins: [
     require('@tailwindcss/typography'),
