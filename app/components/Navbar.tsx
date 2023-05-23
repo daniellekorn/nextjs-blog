312516b5e25765ee9aec36725e753198d66d5126
Danielle Korn <daniella.korn@gmail.com>
2023-05-23 16:26:37 +0300

    start to style nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav>
-            <div>
-                <h1>
-                    <Link href="/">Danielle Korn</Link>
+        <nav className="flex p-4 sticky top-0 drop-shadow-xl z-10">
+            <div className="flex justify-between">
+                <h1 className="justify-start">
+                    <Link href="/" className="text-teal-600">Danielle Korn</Link>
                 </h1>
-                <div>
-                    <Link href="/about">About</Link>
-                    <Link href="/posts">Blog</Link>
-                    <Link href="/contact">Contact</Link>
+                <div className="flex flex-row justify-center gap-6">
+                    <Link href="/about" className="hover:text-teal-400">About</Link>
+                    <Link href="/posts" className="hover:text-teal-400">Blog</Link>
+                    <Link href="/contact" className="hover:text-teal-400">Contact</Link>
                 </div>
             </div>
         </nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="font-sans flex flex-col justify-center items-center p-8">
         <Navbar/>
         {children}
       </body>
