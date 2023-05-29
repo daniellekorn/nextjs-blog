8917434bd6e21d766524abca7c0688ed5025229e
Danielle Korn <daniella.korn@gmail.com>
2023-05-29 14:59:48 +0300

    create cards for recent posts

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,17 +1,28 @@
-import { getSortedPostsData } from "@/lib/posts"
-import PostListItem from "./PostListItem"
+import { getSortedPostsData } from "@/lib/posts";
+import PostListItem from "./PostListItem";
 
 export default function Posts() {
-    const posts = getSortedPostsData()
+  const posts = getSortedPostsData();
 
-    return (
-        <section>
-            <h2>Recent posts</h2>
-            <ul>
-                {posts.map(post => (
-                    <PostListItem key={post.id} post={post} />
-                ))}
-            </ul>
-        </section>
-    )
+  return (
+    <section className="py-8">
+      <h2 className="mb-4 text-xl font-bold text-center lg:text-2xl">
+        Recent posts
+      </h2>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+        {posts.map((post) => (
+          <div key={post.id} className="p-6 bg-white rounded-lg shadow-lg">
+            <h3 className="mb-2 text-lg font-bold">{post.title}</h3>
+            <p className="mb-4 text-gray-700">{post.date}</p>
+            <a
+              href={`/posts/${post.id}`}
+              className="inline-block font-semibold text-teal-500 hover:text-teal-600"
+            >
+              Read more
+            </a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
