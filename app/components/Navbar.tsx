149056da8a6fa0b6b56b16055d83aa02f578bc53
Danielle Korn <daniella.korn@gmail.com>
2023-05-29 10:18:32 +0300

    fix nav bar width and responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 flex flex-wrap items-center justify-between w-full px-24 py-4 bg-white border-b">
-            <div className="flex items-center mr-4 bold">
-                <Link href="/">Danielle Korn</Link>
-            </div>
-            <div className="flex items-center mt-4 md:mt-0">
-                    <Link href="/about" className="block mt-2 mr-4 md:inline-block md:mt-0">About</Link>
-                    <Link href="/posts" className="block mt-2 mr-4 md:inline-block md:mt-0">Blog</Link>
-                    <Link href="/contact" className="block mt-2 mr-4 md:inline-block md:mt-0">Contact</Link>
+        <nav className="fixed top-0 left-0 right-0 flex items-center justify-center w-full bg-white border-b">
+            <div className="container flex flex-wrap items-center justify-between px-4 py-4 mx-auto max-w-8xl">
+                <div>
+                    <Link href="/" className="text-lg font-bold leading-tight text-teal-600 md:inline-block md:mt-0 hover:text-teal-800">Danielle Korn</Link>
+                </div>
+                <div className="flex items-center mt-4 md:mt-0">
+                    <Link href="/about" className="block mt-2 mr-6 text-lg font-bold leading-tight text-teal-800 md:inline-block md:mt-0 hover:text-teal-600">About</Link>
+                    <Link href="/posts" className="block mt-2 mr-6 text-lg font-bold leading-tight text-teal-800 md:inline-block md:mt-0 hover:text-teal-600">Blog</Link>
+                    <Link href="/contact" className="block mt-2 mr-6 text-lg font-bold leading-tight text-teal-800 md:inline-block md:mt-0 hover:text-teal-600">Contact</Link>
+                </div>
             </div>
         </nav>
     )
