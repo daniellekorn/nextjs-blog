5f76e9632d6e0b52cf6d0100c629bdd00affe408
Danielle Korn <daniella.korn@gmail.com>
2023-05-29 10:22:50 +0300

    fix text size for nav bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,12 +5,12 @@ export default function Navbar() {
         <nav className="fixed top-0 left-0 right-0 flex items-center justify-center w-full bg-white border-b">
             <div className="container flex flex-wrap items-center justify-between px-4 py-4 mx-auto max-w-8xl">
                 <div>
-                    <Link href="/" className="text-lg font-bold leading-tight text-teal-600 md:inline-block md:mt-0 hover:text-teal-800">Danielle Korn</Link>
+                    <Link href="/" className="font-bold leading-tight text-teal-600 md:text-lg lg:text-xl md:inline-block md:mt-0 hover:text-teal-800">Danielle Korn</Link>
                 </div>
                 <div className="flex items-center mt-4 md:mt-0">
-                    <Link href="/about" className="block mt-2 mr-6 text-lg font-bold leading-tight text-teal-800 md:inline-block md:mt-0 hover:text-teal-600">About</Link>
-                    <Link href="/posts" className="block mt-2 mr-6 text-lg font-bold leading-tight text-teal-800 md:inline-block md:mt-0 hover:text-teal-600">Blog</Link>
-                    <Link href="/contact" className="block mt-2 mr-6 text-lg font-bold leading-tight text-teal-800 md:inline-block md:mt-0 hover:text-teal-600">Contact</Link>
+                    <Link href="/about" className="block mt-2 mr-6 font-bold leading-tight text-teal-800 md:text-lg lg:text-xl md:inline-block md:mt-0 hover:text-teal-600">About</Link>
+                    <Link href="/posts" className="block mt-2 mr-6 font-bold leading-tight text-teal-800 md:text-lg lg:text-xl md:inline-block md:mt-0 hover:text-teal-600">Blog</Link>
+                    <Link href="/contact" className="block mt-2 mr-6 font-bold leading-tight text-teal-800 md:text-lg lg:text-xl md:inline-block md:mt-0 hover:text-teal-600">Contact</Link>
                 </div>
             </div>
         </nav>
